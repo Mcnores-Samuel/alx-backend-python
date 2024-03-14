bcbb8ffd835d6e2869c6e1c9557e88f4ce58d6b3
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-15 00:58:43 +0200

    takes a float n as argument and returns the floor of the float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This module contains a function floor which takes
+a float n as argument and returns the floor of the float.
+"""
+from math import floor as flo
+
+
+def floor(n: float) -> int:
+    """takes a float n as argument and
+    returns the floor of the float.
+    """
+    return flo(n)
