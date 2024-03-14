65f453d2fad7056c9a3d2b1e4a1513f5906a6511
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-15 00:37:27 +0200

    takes a float a and a float b as arguments and returns their sum as a float.

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+"""This module contains the add function that adds
+float numbers
+"""
+
+
+def add(a: float, b: float) -> float:
+    """takes a float a and a float b as arguments and
+    returns their sum as a float.
+
+    args:
+      a: float number
+      b: float number
+
+    Returns: the sum of two float numbers
+    """
+    return a + b
