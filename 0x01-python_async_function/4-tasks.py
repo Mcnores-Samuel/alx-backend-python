69b17f654191696a259b463d104f39fb4638da36
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-21 23:22:33 +0200

    takes an integer max_delay and returns a asyncio.Task.

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,7 +10,7 @@ import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Takes in 2 int arguments (in this order): n and max_delay and
     spawns wait_random n times with the specified max_delay
 
