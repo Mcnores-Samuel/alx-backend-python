666685678468a7a630c91c0806b6b59e275bcd43
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-21 02:51:19 +0200

    return the list of all the delays (float values).

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,16 +5,19 @@ wait_random n times with the specified max_delay
 """
 from typing import List
 import asyncio
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[int]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Takes in 2 int arguments (in this order): n and max_delay and
     spawns wait_random n times with the specified max_delay
 
     returns the list of all the delays (float values) in ascending order.
     """
-    result: list = asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
+    result: list = []
+    for i in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        result.append(await task)
     return sorted(result)
