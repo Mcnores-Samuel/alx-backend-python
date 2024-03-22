b6dfd7e6a083620927c7e90dd686a451e20dc982
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-03-23 00:37:30 +0200

    async compressions

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:
     """Async Generator function that yields random numbers"""
     for _ in range(10):
         await asyncio.sleep(1)
